d0d4b16be94d28d7ba153d563330a8b719d3bd48
EvsikovaN <evsikova32@gmail.com>
2022-03-09 16:35:44 +0300

    правки после ревью

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  const editProfile = document.querySelector(".edit-profile");
-  const popup = document.querySelector(".popup");
-  const closeButton = document.querySelector(".popup__close");
+  const editProfile = document.querySelector(".profile__btn_edit");
+  const popup = document.querySelector(".popup_type_edit-profile");
+  const closeButton = document.querySelector(".popup__close_form_edit-profile");
   const profileName = document.querySelector(".profile__name");
   const profileDetail = document.querySelector(".profile__detail");
   const inputName = document.querySelector(".popup__input_name");
   const inputDetail = document.querySelector(".popup__input_detail");
-  const formPopup = document.querySelector(".popup__form");
+  const formPopup = document.querySelector(".popup__form_type_edit-profile");
 
-  editProfile.addEventListener("click", openPopup);
-  closeButton.addEventListener("click", closePopup);
-  formPopup.addEventListener("submit", formSubmitHandler);
+  let nameContent;
+  let detailContent;
+  let valueInputName;
+  let valueInputDetail;
 
   function openPopup() {
     popup.classList.add("popup_opened");
@@ -22,21 +23,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
   function fillForm() {
-    let nameContent = profileName.textContent;
-    let detailContent = profileDetail.textContent;
+    //получили то что записано в профиле
+    nameContent = profileName.textContent;
+    detailContent = profileDetail.textContent;
 
+    //заполнили этими значениями поля формы
     inputName.value = nameContent;
     inputDetail.value = detailContent;
   }
 
   function formSubmitHandler(evt) {
     evt.preventDefault();
-    let valueInputName = inputName.value;
-    let valueInputDetail = inputDetail.value;
+    //получили новые значения, которые ввел пользователь
+    valueInputName = inputName.value;
+    valueInputDetail = inputDetail.value;
 
+    //записали их в профиль
     profileName.textContent = valueInputName;
     profileDetail.textContent = valueInputDetail;
 
     closePopup();
   }
+
+  editProfile.addEventListener("click", openPopup);
+  closeButton.addEventListener("click", closePopup);
+  formPopup.addEventListener("submit", formSubmitHandler);
 });
